23c12da6d13ebde0646d3e16e0ac4cf945106574
Karina Lisniak <karyna.lisniak@gmail.com>
2019-05-06 13:02:41 +0300

    Setup packages

---
+++ SaveBridge.BusinessLogic/Services/Interfaces/IStatisticService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SaveBridge.BusinessLogic.Services.Interfaces
+{
+    public interface IStatisticService
+    {
+        void GetBuildingBreakdownStatistic(Guid buildingId);
+
+        void GetBreakdownStatistic();
+
+        void GetBuildingTemperatureStatistic(Guid buildingId);
+
+        void GetBuildingVibrationStatistic(Guid buildingId);
+    }
+}
---
+++ SaveBridge.BusinessLogic/Services/StatisticService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SaveBridge.BusinessLogic.Services.Interfaces;
+
+namespace SaveBridge.BusinessLogic.Services
+{
+    class StatisticService : IStatisticService
+    {
+        public void GetBreakdownStatistic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBuildingBreakdownStatistic(Guid buildingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBuildingTemperatureStatistic(Guid buildingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBuildingVibrationStatistic(Guid buildingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
